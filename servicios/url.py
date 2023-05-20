6e281c50aa16b5a887ddf270566f95cca4620f13
rejota196 <raulej196@gmail.com>
2023-05-20 00:44:06 -0300

    Agregando vista activar registro empleado

--- servicios/url.py
+++ servicios/url.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     # Ruta de la página de inicio
     path('', views.index, name='index'),
-    path('empleados/nuevo',views.registrar_empleado,name="registrar_empleado")
+    path('empleados/nuevo',views.registrar_empleado,name="registrar_empleado"),
+    path('empleados/activar/<int:id>', views.activar_registro_empleado, name="activar_registro_empleado")
 ]
