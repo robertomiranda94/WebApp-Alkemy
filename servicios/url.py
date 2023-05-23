450afe46fd077f4b1b94b328b02f6ed6ae9139bd
robertomiranda94 <88645809+robertomiranda94@users.noreply.github.com>
2023-05-23 19:07:25 -0300

    Revert "Sc4s1-28   crear la view que permita desactivar un registro de empleado"

--- servicios/url.py
+++ servicios/url.py
@@ -2,11 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # Ruta de la página de inicio
-    path('', views.index, name='index'),
-    path('empleados/nuevo',views.registrar_empleado,name="registrar_empleado"),
-    path('empleados/listado/', views.listar_empleados, name='listar_empleados'),
-    path('empleados/activar/<int:id>', views.activar_registro_empleado, name="activar_registro_empleado"),
-    path('empleados/actualizar/<int:id_empleado>', views.actualizar_empleado, name='actualizar_empleado'),
-    path('empleados/desactivar/<int:id>', views.desactivar_empleado, name='desactivar_empleado'),
+    path('', views.index, name='index'), # Ruta de la página de inicio
 ]
