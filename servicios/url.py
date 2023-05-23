6d6d885032937c5bc7b1d7e690ef8cef65852cdb
Roberto <mirandaroberto1994@gmail.com>
2023-05-23 20:05:21 -0300

    crea view para desactivar registro de empleado

--- servicios/url.py
+++ servicios/url.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('empleados/nuevo',views.registrar_empleado,name="registrar_empleado"),
     path('empleados/listado/', views.listar_empleados, name='listar_empleados'),
     path('empleados/activar/<int:id>', views.activar_registro_empleado, name="activar_registro_empleado"),
-    path('empleados/actualizar/<int:id_empleado>', views.actualizar_empleado, name='actualizar_empleado')
+    path('empleados/actualizar/<int:id_empleado>', views.actualizar_empleado, name='actualizar_empleado'),
+    path('empleados/desactivar/<int:id>', views.desactivar_empleado, name='desactivar_empleado'),
 ]
