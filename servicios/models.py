b378dbee02130be4292567bf17704e8d865fdde5
ZelayaGonzalo <zelayagonzalo33@gmail.com>
2023-05-27 22:44:05 -0300

    Añadido metodo _str_  a los modelos

--- servicios/models.py
+++ servicios/models.py
@@ -5,26 +5,35 @@ class Cliente(models.Model):
     nombre = models.CharField(max_length=100)
     apellido = models.CharField(max_length=100)
     activo = models.BooleanField(default=True)
+    
+    def __str__(self):
+         return self.nombre +' '+ self.apellido
 
 class Empleado(models.Model):
     nombre = models.CharField(max_length=50)
     apellido = models.CharField(max_length=50)
     numero_legajo = models.CharField(max_length=50)
     activo = models.BooleanField(default=True)
-
+    def __str__(self):
+         return self.nombre +' '+ self.apellido
 
 class Servicio(models.Model):
-	nombre = models.CharField(max_length=30)
-	descripcion = models.CharField(max_length=200)
-	precio = models.FloatField(default=0.0)
-	activo = models.BooleanField(default=True)
-
+    nombre = models.CharField(max_length=30)
+    descripcion = models.CharField(max_length=200)
+    precio = models.FloatField(default=0.0)
+    activo = models.BooleanField(default=True)
+    def __str__(self):
+         return self.nombre
+        
+        
 class Coordinador(models.Model):
     nombre = models.CharField(max_length=20)
     apellido = models.CharField(max_length=20)
     numero_documento = models.IntegerField(blank=False)
     fecha_alta = models.DateTimeField(auto_now_add=True, blank=True)
     activo = models.BooleanField(default=True)
+    def __str__(self):
+         return self.nombre +' '+ self.apellido
     
 class ReservaServicio(models.Model):
     fecha_creacion = models.DateField(auto_now_add=True)
