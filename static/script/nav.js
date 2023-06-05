f6fb091b50ee4199098e47d4f4b4b063989f3379
ZelayaGonzalo <zelayagonzalo33@gmail.com>
2023-06-05 15:03:26 -0300

    Añadida botón para mostrar sidebar cuando no hay espacio

--- static/script/nav.js
+++ static/script/nav.js
@@ -3,6 +3,12 @@ let secondLevelLocation = pathArray[1];
 if(secondLevelLocation == ""){
     secondLevelLocation = "dashboard"
 }
-
 let linkActivo = document.getElementById(secondLevelLocation)
 linkActivo.classList.add('seleccionado')
+
+
+
+function mostrar_sidebar(){
+    let sidebar = document.getElementById("sidebar")
+    sidebar.classList.toggle('mostrar')
+}
