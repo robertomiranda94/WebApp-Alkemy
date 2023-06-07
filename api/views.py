466d583035b0e1f552532404dd1b19d2ab847c1c
ZelayaGonzalo <zelayagonzalo33@gmail.com>
2023-06-07 16:58:26 -0300

    Añadidos endpoints para listados

--- api/views.py
+++ api/views.py
@@ -1,9 +1,8 @@
 from django.http import JsonResponse
 
-from servicios.models import Servicio 
+from servicios.models import Servicio, Cliente, Empleado, Coordinador
 
 
-# Create your views here.
 def listar_servicios_json(request):
     servicios = Servicio.objects.all()
     servicios_data = []
@@ -29,3 +28,45 @@ def mostrar_servicio_json(request,id):
         return JsonResponse(servicio_json)
     except Exception as error:
         return JsonResponse([],safe=False)
+    
+
+def listar_clientes_json(request):
+    clientes = Cliente.objects.all()
+    clientes_data = []
+    for cliente in clientes:
+        cliente_data = {
+            "id": cliente.id,
+            "nombre": cliente.nombre,
+            "apellido": cliente.apellido,
+        }
+        clientes_data.append(cliente_data)
+    return JsonResponse(clientes_data, safe = False) #safe=False permite que se devuelva una lista en lugar de un diccionario.
+
+
+def listar_empleados_json(request):
+    empleados = Empleado.objects.all()
+    empleados_data = []
+    for empleado in empleados:
+        empleado_data = {
+            "id": empleado.id,
+            "nombre": empleado.nombre,
+            "apellido": empleado.apellido,
+            "numero_legajo":empleado.numero_legajo
+        }
+        empleados_data.append(empleado_data)
+    return JsonResponse(empleados_data, safe = False) #safe=False permite que se devuelva una lista en lugar de un diccionario.
+
+
+def listar_coordinadores_json(request):
+    coordinadores = Coordinador.objects.all()
+    coordinadores_data = []
+    for coordinador in coordinadores:
+        coordinador_data = {
+            "id": coordinador.id,
+            "nombre": coordinador.nombre,
+            "apellido": coordinador.apellido,
+            "numero_documento":coordinador.numero_documento,
+            "fecha_alta":coordinador.fecha_alta
+        }
+        coordinadores_data.append(coordinador_data)
+    return JsonResponse(coordinadores_data, safe = False) #safe=False permite que se devuelva una lista en lugar de un diccionario.
